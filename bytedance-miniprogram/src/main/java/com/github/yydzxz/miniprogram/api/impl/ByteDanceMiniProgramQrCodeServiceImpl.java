8103c412931716cbae86b90d4bb888b560bde3a6
Gadfly <gadfly@gadfly.vip>
2022-01-12 17:44:02 +0800

    style: unused import

--- bytedance-miniprogram/src/main/java/com/github/yydzxz/miniprogram/api/impl/ByteDanceMiniProgramQrCodeServiceImpl.java
+++ bytedance-miniprogram/src/main/java/com/github/yydzxz/miniprogram/api/impl/ByteDanceMiniProgramQrCodeServiceImpl.java
@@ -13,7 +13,6 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
